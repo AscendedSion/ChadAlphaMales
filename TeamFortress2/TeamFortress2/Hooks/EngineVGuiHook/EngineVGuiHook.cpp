60745ea61a7353340236919e834f5eeec8a79cf9
buzing <107199671+buzing@users.noreply.github.com>
2023-08-21 16:30:37 +0200

    probably wrong?
    idk if this works i forgot to commit this on my puter

--- TeamFortress2/TeamFortress2/Hooks/EngineVGuiHook/EngineVGuiHook.cpp
+++ TeamFortress2/TeamFortress2/Hooks/EngineVGuiHook/EngineVGuiHook.cpp
@@ -181,7 +181,7 @@ void __stdcall EngineVGuiHook::Paint::Hook(int mode)
 				int ms = std::max(0, (int)std::round(g_GlobalInfo.m_Latency * 1000.f));
 				// Change admin to something else if you really need to, it was for the protection thing
 				//_snprintf(buff, sizeof(buff), _("CAM [v1.5] | fps: %i | delay: %ims") ,fps, ms);
-				std::string waterMark = tfm::format(_(" f:%d | d:%ims"), fps, ms);
+				std::string waterMark = tfm::format(_("%s"), (__DATE__));
 				std::wstring waterMarkW(waterMark.begin(), waterMark.end());
 
 				g_Interfaces.Surface->GetTextSize(g_Draw.m_vecFonts[FONT_MENU].dwFont, waterMarkW.c_str(), label_w, label_h);
@@ -197,9 +197,9 @@ void __stdcall EngineVGuiHook::Paint::Hook(int mode)
 						//ms = 1.f / nci->GetLatency(FLOW_INCOMING);
 						//ms = std::max(0, (int)std::round(g_GlobalInfo.m_Latency * 1000.f));
 
-						g_Draw.Rect(width - 10 - boxw, 11, boxw, 18, { 240, 110, 140, 130 });
+						g_Draw.Rect(width - 10 - boxw, 11, boxw, 18, { 30, 30, 30, 127 });
 						//g_Draw.Rect(width - 10 - boxw, 10, boxw, 2, { 240, 110, 140, 130 });
-						g_Draw.String(FONT_ESP, width - 10 - boxw + 5, 20, { 240, 160, 180, 250 }, ALIGN_CENTERVERTICAL, waterMarkW.c_str());
+						g_Draw.String(FONT_ESP, width - 10 - boxw + 5, 20, { 255, 255, 255}, ALIGN_CENTERVERTICAL, waterMarkW.c_str());
 					}
 				}
 			}
